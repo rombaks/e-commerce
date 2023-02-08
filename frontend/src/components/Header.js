4cbdfaf47fdc36c1fac28815f288da6cf87b15b3
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-08 16:52:02 +0400

    Change href to LinkContainer in Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
-      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">SkyStore</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>SkyStore</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/cart/"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-              <Nav.Link href="/login/"><i className='fas fa-user'></i> Login</Nav.Link>
+              <LinkContainer to="/cart/">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              
+              <LinkContainer to="/login/">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;

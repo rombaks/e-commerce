82833aa165551dbc585bcaabf9362ddfe49c0e0c
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-08 18:18:48 +0400

    Add Go Back button in ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,9 @@ import React from 'react'
 function ProductScreen() {
   return (
     <div>
-      Product
+      <Link to="/" className="btn btn-light my-3">
+        Go Back
+      </Link>
     </div>
   )
 }

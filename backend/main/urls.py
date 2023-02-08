4872557f23b33e61bef4cdc313e0bdc48942ce55
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-09 00:16:31 +0400

    Install Django, DRF, flake8, black, isort

---
+++ backend/main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from main.views import get_product, get_products, get_routes
+
+urlpatterns = [
+    path("", get_routes, name="routes"),
+    path("products/", get_products, name="products"),
+    path("products/<str:pk>", get_product, name="product"),
+]

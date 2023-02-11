7fbf8cca9f5016615a9b19f3d3b5df16b97bd92d
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-11 11:01:32 +0400

    Add cart data in loacel storage in cartActions.js

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -15,4 +15,6 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       qty,
     },
   });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };

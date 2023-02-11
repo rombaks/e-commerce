9acca3de10c984ab4c3fc4cb010cf19ea5aab70e
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-11 13:31:15 +0400

    Add useEffect to add ToCart action in CartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Link, useSearchParams, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { Message } from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
 function CartScreen() {
-  return (
-    <div>
-      Cart
-    </div>
-  )
+  const { id } = useParams();
+  const [searchParams] = useSearchParams();
+  const qty = Number(searchParams.get("qty"))
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]);
+
+  return <div>Cart</div>;
 }
 
-export default CartScreen
+export default CartScreen;

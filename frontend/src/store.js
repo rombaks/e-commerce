312852ee1d47a63bee0c9316cf19bbd7bc6cf7a7
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-11 12:32:10 +0400

    Add CartItemsFromStorage to initialState in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,12 @@ const reducer = combineReducers({
   cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems") ?
+  JSON.parse(localStorage.getItem("cartItems")) : []
+
+const initialState = {
+  cart: {cartItems: cartItemsFromStorage}
+};
 
 const middleware = [thunk];
 

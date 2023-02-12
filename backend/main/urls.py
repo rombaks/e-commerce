66c1467c5457833defb5c111fe4f613f07799003
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-12 15:36:49 +0400

    Add "users/login/" path in urls.py

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from main.views import get_product, get_products, get_routes
+from main.views import MyTokenObtainPairView, get_product, get_products, get_routes
 
 
 urlpatterns = [
     path("", get_routes, name="routes"),
     path("products/", get_products, name="products"),
     path("products/<str:pk>", get_product, name="product"),
+    path("users/login/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]

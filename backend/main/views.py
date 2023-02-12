92a653fd7721ff443236b6233d7d31ef4d6a216b
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-12 15:36:03 +0400

    Add  MyTokenObtainPairView in views.py

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,8 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Product
 from .serializers import ProductSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, user):
+        data = super().validate(user)
+
+        data["username"] = self.user.username
+        data["email"] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(["GET"])

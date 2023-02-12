8f9591e15c83816b948030eb3f8d8f2d0d12f12c
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-12 21:44:40 +0400

    Add get_users with IsAdminUser perm in views.py

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,9 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .models import Product
+from .models import Product, User
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 
@@ -22,9 +23,18 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def get_user(request):
     user = request.user
     serirializer = UserSerializer(user, many=False)
+    return Response(serirializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    users = User.objects.all()
+    serirializer = UserSerializer(users, many=True)
     return Response(serirializer.data)
 
 

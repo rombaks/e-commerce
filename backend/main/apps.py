640e3ed5a98e6ae73344a625bf546f1ef5e76c2d
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-13 17:59:18 +0400

    Add main.signals in apps.py

--- backend/main/apps.py
+++ backend/main/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "main"
+
+    def ready(self) -> None:
+        from main import signals  # noqa
+
+        return super().ready()

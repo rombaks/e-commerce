87832b59ba59b380a22c6994908d48af5756f1a4
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-14 12:05:28 +0400

    Separate user_views form views.py

---
+++ backend/main/views/user_views.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.hashers import make_password
+
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from main.models import User
+from main.serializers import UserSerializer, UserSerializerWithToken
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        data.update(serializer)
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(["POST"])
+def register_user(request):
+    data = request.data
+
+    if data:
+        user = User.objects.create(
+            first_name=data["name"],
+            username=data["email"],
+            email=data["email"],
+            password=make_password(data["password"]),
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    else:
+        message = {"detail": "User with this email already exists"}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_user(request):
+    user = request.user
+    serirializer = UserSerializer(user, many=False)
+    return Response(serirializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    users = User.objects.all()
+    serirializer = UserSerializer(users, many=True)
+    return Response(serirializer.data)

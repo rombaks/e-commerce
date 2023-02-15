0802f1e5c0acf00377e3bdc296af9bce39e73546
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-15 16:45:38 +0400

    Add profile/update/ in user_urls.py

--- backend/main/urls/user_urls.py
+++ backend/main/urls/user_urls.py
@@ -5,12 +5,14 @@ from main.views.user_views import (
     get_user,
     get_users,
     register_user,
+    update_user,
 )
 
 
 urlpatterns = [
     path("", get_users, name="users"),
+    path("register/", register_user, name="register"),
     path("login/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("profile/", get_user, name="user"),
-    path("register/", register_user, name="register"),
+    path("profile/update/", update_user, name="update"),
 ]
